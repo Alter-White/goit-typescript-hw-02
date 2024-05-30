11cfff263e7f5bdcec2cdd94c564750efa8be76a
Ivan <ivanprime96@gmail.com>
2024-05-31 00:47:06 +0300

    api fix

--- src/components/Api/api.ts
+++ src/components/Api/api.ts
@@ -1,35 +1,20 @@
 import axios, { AxiosResponse } from "axios";
+import { PicsResponse } from "../../App";
 
 const ACCESS_KEY = "bC9Y0JD6XZ2qTWbs8qQwT_4PTroYWKuL_hxdm6je2to";
-
-interface UnsplashResponse {
-  results: UnsplashPhoto[];
-}
-
-interface UnsplashPhoto {
-  id: string;
-  urls: {
-    regular: string;
-  };
-}
 
 export const requestPicsByQuery = async (
   query: string,
   page: number
-): Promise<UnsplashResponse> => {
-  try {
-    const { data }: AxiosResponse<UnsplashResponse> = await axios.get(
-      `https://api.unsplash.com/search/photos?page=${page}&query=${query}&per_page=20`,
-      {
-        headers: {
-          Authorization: `Client-ID ${ACCESS_KEY}`,
-        },
-      }
-    );
+): Promise<PicsResponse> => {
+  const response: AxiosResponse<PicsResponse> = await axios.get(
+    `https://api.unsplash.com/search/photos?page=${page}&query=${query}&per_page=20`,
+    {
+      headers: {
+        Authorization: `Client-ID ${ACCESS_KEY}`,
+      },
+    }
+  );
 
-    return data;
-  } catch (error) {
-    console.error("An error occurred while making the request:", error);
-    throw error;
-  }
+  return response.data;
 };
